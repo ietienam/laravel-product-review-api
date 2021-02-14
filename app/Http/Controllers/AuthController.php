85fce2a0b100cdaeebf3a3e10129774be40ecb4c
Ini Etienam <ietienam@gmail.com>
2021-02-14 13:17:43 +0100

    product resourse route next

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,8 +3,88 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use App\Models\User;
 
 class AuthController extends Controller
 {
-    //
+    public function __construct()
+    {
+        $this->middleware('auth')->except(['register', 'login']);
+    }
+
+    public function register(Request $request)
+    {
+        $validator = Validator::make($request
+            ->only(['name', 'email', 'password']), [
+            'name' => 'required|string|between:2,100',
+            'email' => 'required|string|email|max:100|unique:users',
+            'password' => 'required|string|min:6',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'status' => false,
+                'message' => $validator->errors()->toJson()
+            ], 400);
+        }
+
+        $user = User::create(array_merge(
+            $validator->validated(),
+            ['password' => bcrypt($request->password)]
+        ));
+
+        $token = auth()->login($user);
+
+        return $this->respondWithToken($token);
+    }
+
+    public function login(Request $request)
+    {
+        $validator = Validator::make($request
+            ->only(['email', 'password']), [
+            'email' => 'required|email',
+            'password' => 'required|string|min:6',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'status' => false,
+                'message' => $validator->errors()
+            ], 400);
+        }
+
+        if (!$token = auth()->attempt($validator->validated())) {
+            return response()->json([
+                'status' => false,
+                'error' => 'Unauthorized! Invalid email or password'
+            ], 401);
+        }
+
+        return $this->respondWithToken($token);
+    }
+
+    protected function respondWithToken($token)
+    {
+        return response()->json([
+            'access_token' => $token,
+            'token_type' => 'bearer',
+            'expires_in' => auth()->factory()->getTTL() * 60
+        ]);
+    }
+
+    /**
+     * Get the authenticated user
+     *
+     */
+    public function me()
+    {
+        return response()->json(auth()->user());
+    }
+
+    public function logout()
+    {
+        auth()->logout();
+        return response()->json(["message" => "Successfully logged out"]);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::get('me', 'AuthController@me');
+Route::post('login', 'AuthController@login');
+Route::post('register', 'AuthController@register');
+Route::post('logout', 'AuthController@logout');
